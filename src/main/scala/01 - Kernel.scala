d1c3670e485a7317625614dad57e56b84151579a
Vincent KUBICKI <vincent.kubicki@gmail.com>
2017-07-29 13:15:20 +0200

    Simple kernel and Gram matrix computation added

---
+++ src/main/scala/01 - Kernel.scala
@@ -0,0 +1,9 @@
+import breeze.linalg._
+
+/**
+ * Biobliography:
+ * -	https://en.wikipedia.org/wiki/Reproducing_kernel_Hilbert_space#Examples
+ */
+object Kernel {
+  def linear(x: DenseVector[Double], y: DenseVector[Double]): Double = x dot y
+}
---
+++ src/main/scala/02 - Gram.scala
@@ -0,0 +1,27 @@
+import breeze.linalg._
+
+object Gram {
+  /**
+   * Observations are presented in a matrix, where each row is an observation, and each
+   * column is a variable. The kernel is passed as an argument.
+   * */
+  def generate(
+      observations: DenseMatrix[Double],
+      kernel: (DenseVector[Double], DenseVector[Double]) => Double)
+  : DenseMatrix[Double] = {
+    val nObs = observations.rows
+    
+    return DenseMatrix.tabulate[Double](nObs, nObs)((i, j) => kernel(observations(i, ::).t, observations(j, ::).t))
+  }
+  
+  def generateTest {
+    val obs = DenseMatrix(
+        (1.0, 2.0),
+        (3.0, 4.0),
+        (5.0, 6.0))
+        
+    val gram = generate(obs, Kernel.linear)
+    
+    println(gram)
+  }
+}
