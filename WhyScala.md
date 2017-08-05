9851f448ba9f494c5fa2ec230c732bcc717f1f8f
Vincent KUBICKI <vincent.kubicki@gmail.com>
2017-08-05 16:15:05 +0200

    Data generation using Gaussian Mixture added

---
+++ WhyScala.md
@@ -0,0 +1,9 @@
+Why Scala - Vincent KUBICKI - 2017
+
+# Abstract
+
+This document summarizes the experience of doign numerical computations in Scala. It is based on my experience coding this library. I will not include external ressources or researches. Coming from a math / physics background and not a CS background, I lack a complete picture of the type system, category theory and such. This is really a description of the applied.
+
+# Pro
+
+# Cons
---
+++ src/main/scala/p02kmeans/01 - Data.scala
@@ -0,0 +1,36 @@
+package p02kmeans
+
+import breeze.linalg._
+import breeze.stats.distributions._
+
+/**
+ * First data generation is simple, gaussian mixture model.
+ */
+object Data {
+  class GaussianClassParam (
+      val mean: Double,
+      val sd: Double)
+  
+  /**
+   * Gaussian Mixture Model is a 
+   * 
+   * @param proportion marginal probability for each label
+   * @param param param[j][k] returns the GaussianClassParam for the k class of the j variable
+   * @param nObs number of observations to be generated
+   * @result DenseMatrix_ij where i is the observation index and j is the variable number
+   */
+  def gaussianMixture(
+      proportion: DenseVector[Double],
+      param: Vector[Vector[GaussianClassParam]],
+      nObs: Int)
+  : DenseMatrix[Double] = {
+    val nVar = param.size
+    val multiSampler = Multinomial(proportion) // sampler for the latent class
+    val varSamplerVec = param.map(v => v.map(c => Gaussian(c.mean, c.sd)))
+    val zi = DenseVector.fill[Int](nObs)(multiSampler.sample)
+
+    val data = DenseMatrix.tabulate[Double](nObs, nVar)((i, j) => varSamplerVec(j)(zi(i)).sample)
+    
+    return data
+  }
+}
